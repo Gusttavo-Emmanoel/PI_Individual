d6e8a8c24b557dff763505b55e9ef8c9504b7f5d
Gusttavo-Emmanoel <gustavo.aferreira@sptech.school>
2025-11-30 20:26:01 -0300

    Feat: Cadastro backend

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -8,18 +8,18 @@ function autenticar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-// function cadastrar(nome, email, senha, fkEmpresa) {
+function cadastrar(nome, email, senha) {
 
-//     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-//     //  e na ordem de inserção dos dados.
-//     var instrucaoSql = `
-//         INSERT INTO usuario (nome, email, senha, fk_empresa) VALUES ('${nome}', '${email}', '${senha}', '${fkEmpresa}');
-//     `;
-//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
-//     return database.executar(instrucaoSql);
-// }
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO usuario (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
 
 module.exports = {
     autenticar,
-    //cadastrar
+    cadastrar
 };
